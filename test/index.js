a21322c47bc40b99218d66445fe8a817537ed5d1
Andrej <antandrej2003@gmail.com>
2023-04-08 15:43:11 +0200

    Rewriting to work with a database

--- test/index.js
+++ test/index.js
@@ -1,5 +1,8 @@
+const client = require('./connection.js')
 const express = require('express');
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
 app.use(express.json());
 
@@ -7,56 +10,74 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-const movies = [
-    { id: 1, name: "Movie1", genres: ["Drama", "Horror", "Action"]},
-    { id: 2, name: "Movie2", genres: ["Comedy", "Thriller", "Action"]},
-    { id: 3, name: "Movie3", genres: ["Biography", "Action", "Drama"]},
-    { id: 4, name: "Movie4", genres: ["Musical", "Sci-Fi", "War"]},
-]
 
-app.get('/api/movies', (req, res) => {
-    res.send(movies);
+app.get('/api/users', (req, res) => {
+    client.query('SELECT * FROM users', (err, result) =>{
+        if(!err){
+            res.send(result.rows);
+        }
+    });
+    client.end;
 });
 
-app.get('/api/movies/:id', (req, res) => {
-    const movie = movies.find(c => c.id === parseInt(req.params.id));
-    if (!movie) return res.status(404).send("The movie with the given id does not exist!")
-    res.send(movie);
+app.get('/api/users/:id', (req, res) => {
+    client.query(`SELECT * FROM users WHERE id=${req.params.id}`, (err, result) =>{
+        if(!err){
+            res.send(result.rows);
+        }
+    });
+    client.end;
 });
 
-app.post('/api/movies', (req, res) => {
-    if(!req.body.name || !req.body.genres) return res.status(400).send("Name and at least one genre of the movie is required !");
+app.post('/api/users', (req, res) => {
+    const user = req.body;
+    let insertQuery = `INSERT INTO users(id, firstname, lastname, location) VALUES(${user.id}, '${user.firstname}', '${user.lastname}', '${user.location}')`;
 
-    const movie = {
-        id: movies.length + 1,
-        name: req.body.name,
-        genres: req.body.genres
-    }
-    movies.push(movie);
-    res.send(movie);
+    client.query(insertQuery, (err, result) =>{
+        if(!err){
+            res.send('User added!');
+        }
+        else{
+            res.send(err.message);
+        }
+    });
+    client.end;
 });
 
-app.put('/api/movies/:id', (req, res) => {
-    const movie = movies.find(c => c.id === parseInt(req.params.id));
-    if (!movie) return res.status(404).send("The movie with the given id does not exist!")
+app.put('/api/users/:id', (req, res) => {
+    let user = req.body;
+    let updateQuery =   `UPDATE users SET 
+                        firstname = '${user.firstname}', 
+                        lastname = '${user.lastname}', 
+                        location = '${user.location}'
+                        WHERE id = ${user.id}`;
     
-    if(!req.body.name || !req.body.genres) return res.status(400).send("Name and at least one genre of the movie is required !");
-
-    movie.name = req.body.name;
-    movie.genres = req.body.genres;
-
-    res.send(movie);
+    client.query(updateQuery, (err, result) =>{
+        if(!err){
+            res.send('User updated !');
+        }
+        else{
+            res.send(err.message);
+        }
+    });
+    client.end;
 });
 
-app.delete('/api/movies/:id', (req, res) => {
-    const movie = movies.find(c => c.id === parseInt(req.params.id));
-    if (!movie) return res.status(404).send("The movie with the given id does not exist!")
+app.delete('/api/users/:id', (req, res) => {
+    let deleteQuery = `DELETE FROM users WHERE id=${req.params.id}`;
 
-    const index = movies.indexOf(movie);
-    movies.splice(index, 1);
-
-    res.send(movie);
+    client.query(deleteQuery, (err, result) =>{
+        if(!err){
+            res.send('User deleted !');
+        }
+        else{
+            res.send(err.message);
+        }
+    });
+    client.end;
 });
 
 const port = process.env.PORT || 3000;
 app.listen(3000, () => console.log(`Listening on port ${port} . . .`));
+
+client.connect();
